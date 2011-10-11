2135da758ff1214cf339f502fa2e92e487031002
Joakim Carselind <j.carselind@gmail.com>
2011-10-11 10:24:01 +0200

    Added printing for the figures from lab pdf

--- lab3.m
+++ lab3.m
@@ -40,7 +40,7 @@ g = discriminant(test_data(:,1:2), mu, sigma, p)
 [M N] = size(test_data);
 [dummy class] = max(g, [], 2);
 class = class - 1;
-delta = class == test_data(:,end)
+delta = class == test_data(:,end);
 error_test = 1.0-sum(class == test_data(:,end))/M
 
 ax = [0.2 0.5 0.2 0.45];
@@ -91,4 +91,24 @@ T = 6;
 class = adaboost_discriminant(test_data(:,1:N-1), mu, sigma, p, alpha, classes, T);
 boost_error_test = 1.0-sum(class == test_data(:,end))/M
 
+figure;
+hold on;
+plot(data2(:,1), data2(:,2), '.');
+plot(data1(:,1), data1(:,2), '.r');
+legend('Hand holding book', 'Hand');
+ax = [0.2 0.5 0.2 0.45];
+axis(ax);
+x = ax(1):0.01:ax(2);
+y = ax(3):0.01:ax(4);
+[z1 z2] = meshgrid(x, y);
+z1 = reshape(z1, size(z1,1)*size(z1,2), 1);
+z2 = reshape(z2, size(z2,1)*size(z2,2), 1);
+g = adaboost_discriminant([z1 z2], mu, sigma, p, alpha, classes, T);
+gg = reshape(g, length(y), length(x));
+[c,h] = contour(x, y, gg, [0.5 0.5]);
+set(h, 'LineWidth', 3);
 
+tmp = reshape(book_rg, size(book_rg,1)*size(book_rg,2), 2);
+g = adaboost_discriminant(tmp, mu, sigma, p, alpha, classes, T);
+gg = reshape(g, size(book_rg,1), size(book_rg,2));
+mask = gg > 0.5;
