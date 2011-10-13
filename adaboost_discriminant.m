b36da2baffebe5bde26c5dd1abcea53bd73b0b91
Joakim Carselind <j.carselind@gmail.com>
2011-10-13 18:51:39 +0200

    Used GUESS THE DOT and solved the problem. It now works

--- adaboost_discriminant.m
+++ adaboost_discriminant.m
@@ -12,37 +12,15 @@ function [c] = adaboost_discriminant( data, mu, sigma, p, alpha, classes, T)
 %   The discriminant function computes that given a pixel, how likely is it
 %   to belong to the hand image or the hand plus book image
 
-% [M, N] = size(data);
-% c = ones(M, 1);    % Pre-allocate space for c
-% 
-% %c_t = ones(length(classes), 1);
-% c_t = zeros(M,2);
-% for t=1:T
-%     p_ = p(t,:);
-%     g = discriminant(data, mu(:,:,t), sigma(:,:,t), p_);
-%     [~, class_ ] = max(g, [], 2);
-%     class_ = class_ - 1;
-% end
-% for i=1:length(classes)
-%     delta = find(class_ == classes(i));
-%     c_t(i,1) = c_t(i,1) + sum(alpha(t,1) * delta);
-% end
-% c(m,1) = max(c_t,[],1);
-% 
-% end
-
-
-%function g  = discriminant(data, mu, sigma, p)
-
 % data - MxN matrix
 % mu   - CxN
 % sigma- CxN
 % g    - MxC
 % c    - Mx1
 
-N = size(data, 2);   % N - antal dimensioner
-M = size(data,1);  % M - antal datapunkter
-C = size(sigma, 1);% C - antal klasser
+N = size(data, 2);  % N - antal dimensioner
+M = size(data,1);   % M - antal datapunkter
+C = size(sigma, 1); % C - antal klasser
 
 tempC = zeros(M,C);
 
@@ -52,7 +30,7 @@ for i = 1:T
     class = class - 1;
     
     for j = 1:C
-        tempC(:,j) = tempC(:,j) + alpha(i)*delta(class, classes(j));
+        tempC(:,j) = tempC(:,j) + alpha(i) .* (class == classes(j));
     end
 end
 
